8bf069d6dec83d3e62ee8c957d0498ff95b01d17
XIAOLONGLII <1491skdpf89@gmail.com>
2020-07-09 09:49:42 -0400

    user delete update post && show on user navi bar

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from .views import (
+	PostListView,
+	PostDetailView,
+	PostCreateView,
+	PostUpdateView,
+	PostDeleteView,
+)
 from . import views
 # 4.after came here, the page will look after blog/, 
 # 5.since it is empty, then let's look if there is empty in the pattern
 # 6.yes, there is. then where the empty take us. to the view.py fuction home()
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
